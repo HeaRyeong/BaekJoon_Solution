1c084ad87ee3653a86af7eb5f036c3b5b610479a
Hearyeong <ahr0322@naver.com>
2022-04-05 20:50:14 +0900

    4/5
    11718 - 그대로 출력하기
    --> 사용 라이브러리 getline() , string
    --> 공부 필요한 개념 std::basic_istream https://en.cppreference.com/w/cpp/io/basic_istream

---
+++ Algorithm/Algorithm/Baek_11718.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Baek_11718
+{
+public:
+
+	void solution()
+	{
+		string input;
+
+		while (getline(cin, input, '\n'))
+		{
+			cout << input << endl;
+		}
+	}
+};
