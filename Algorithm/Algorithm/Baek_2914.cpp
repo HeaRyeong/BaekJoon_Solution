f733a1fb207a9fba1e65380e99fe5f5d050190e2
Hearyeong <ahr0322@naver.com>
2022-04-06 21:37:45 +0900

    Create Baek_2914.cpp
    4/6 저작권 - 2914
    그냥 사칙연산 문제 ( 키워드 : 올림 , 적어도 )

---
+++ Algorithm/Algorithm/Baek_2914.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+// 그냥 사칙연산 문제
+// 적어도와 올림이 키포인트
+
+void solution_2914()
+{
+	int A, I;
+
+	cin >> A >> I;
+
+	int result = (I - 1) * A + 1;
+
+	cout << result << endl;
+
+}
